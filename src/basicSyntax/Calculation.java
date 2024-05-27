0015929a8b0709201689e185bb50789f0af602f3
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-05-28 03:30:34 +0900

    「四則演算と計算処理」コースの課題提出

---
+++ src/basicSyntax/Calculation.java
@@ -0,0 +1,21 @@
+package basicSyntax;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 5;
+        int y = 10;
+        int z = 5 + 10;
+        System.out.println("変数Zの値は" + z + "です。");
+
+        double a = 5.25;
+        a = a + 2.5;
+        a = a * 3.0;
+        int b = (int)a;
+        b = b + 10;
+        System.out.println("変数aの値は" + a + "です。");
+        System.out.println("変数bの値は" + b + "です。");
+	}
+
+}
