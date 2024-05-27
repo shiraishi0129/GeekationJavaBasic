1d22e08d5d065ae615c987391bc8ffae1856df88
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-05-28 03:21:22 +0900

    「クラス」コースの課題

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int age = 23;
+        double height = 174.5;
+        int num1 = 5;
+        int num2 = 3;
+
+        GeekIntroduction.greeting(age,height);
+        GeekIntroduction.specialSkill(num1, num2);
+        }
+}
+
