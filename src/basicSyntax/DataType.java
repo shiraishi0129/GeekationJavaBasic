136f8d41ea1d5a6f16a0ae758a9f437c9d3521b5
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-05-28 12:35:08 +0900

    インデントの修正

--- src/basicSyntax/DataType.java
+++ src/basicSyntax/DataType.java
@@ -3,7 +3,6 @@ package basicSyntax;
 public class DataType {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("こんにちは" + "私は" +  "Geek君です。");
         System.out.println(10);
         System.out.println("20" + "24");
