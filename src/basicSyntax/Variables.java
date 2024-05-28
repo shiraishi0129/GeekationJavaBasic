d36fd6baba4af3d3ccd8e629f0e33edd3345528b
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-05-28 19:45:21 +0900

    インデントの修正

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -5,11 +5,11 @@ public class Variables {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int num1 = 5;
-        double num2 = 10;
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println("私の好きな整数は" + num1 + "です。");
-        System.out.println("私の好きな少数は" + num2 + "です。");
+		double num2 = 10;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println("私の好きな整数は" + num1 + "です。");
+		System.out.println("私の好きな少数は" + num2 + "です。");
 	}
 
 }
