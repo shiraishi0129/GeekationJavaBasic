9efb28fbdaa9e9984188279d69ba609b8688e217
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-06-03 00:28:57 +0900

    インデントの修正

--- src/basicSyntax/ForWhile.java
+++ src/basicSyntax/ForWhile.java
@@ -5,34 +5,33 @@ public class ForWhile {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		for (int a = 1;a <= 100;a++){
-            if (a % 3 == 0 && a % 5 == 0) {
-                System.out.print(a);
-                System.out.println(":FizzBuzz");
-                continue;
-                }
-            else if (a % 3 == 0) {
-                System.out.print(a);
-                System.out.println(":Fizz");
-                continue;
-                }
-            else if (a % 5 == 0) {
-                System.out.print(a);
-                System.out.println(":Buzz");
-                continue;
-                }
-            }
+			if (a % 3 == 0 && a % 5 == 0) {
+				System.out.print(a);
+				System.out.println(":FizzBuzz");
+				continue;
+            	}
+			else if (a % 3 == 0) {
+				System.out.print(a);
+				System.out.println(":Fizz");
+				continue;
+            	}
+			else if (a % 5 == 0) {
+				System.out.print(a);
+				System.out.println(":Buzz");
+				continue;
+             	}
+         	}
 
-            String[] pg= {"Ruby", "Golang", "Java", "Python", "TypeScript", "HTML", "Javascript"};
-            for(String prg:pg){
-                if (prg == "Java") {
-                    System.out.println("現在学習中の言語はJavaです。");
-                    continue;
-                }
-                else if (prg == "HTML") {
-                    System.out.println("HTMLはプログラミング言語ではありません。");
-                }
-                System.out.println(prg);
-            }
-	}
-
+         	String[] pg= {"Ruby", "Golang", "Java", "Python", "TypeScript", "HTML", "Javascript"};
+         	for(String prg:pg){
+         		if (prg == "Java") {
+         			System.out.println("現在学習中の言語はJavaです。");
+         			continue;
+         		}
+         		else if (prg == "HTML") {
+         			System.out.println("HTMLはプログラミング言語ではありません。");
+         		}
+         		System.out.println(prg);
+         	}
+		}
 }
