0b6032e28aecfc65be33a1bc006653574208c826
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-06-05 14:08:31 +0900

    オブジェクト指向演習問題の提出
    オブジェクト指向演習問題の作成を致しました。
    ご確認よろしくお願いします。

---
+++ src/objectExercise/Apartment.java
@@ -0,0 +1,25 @@
+package objectExercise;
+
+public class Apartment  extends Property{
+	private String layout;
+	
+	public String getLayout() {
+		return layout;
+	}
+	
+	public  void setLayout(String layout) {
+		this.layout = layout;
+	}
+		
+	Apartment(String buildName,String ownerName, String kindName,int Price, String layout){
+		super(buildName, ownerName, kindName, Price);
+		this.layout = layout;
+	}
+
+	
+	public void layoutList() {
+		super.list();
+		System.out.println("間取り:" + getLayout());
+		System.out.println("==========================");
+	}
+}
