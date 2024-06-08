000859baed159e8c851f00f1c317fc5890e25ed8
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-06-09 01:49:15 +0900

    実行結果がおかしかったため、for文内を修正。

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -11,9 +11,8 @@ public class GeekIntroduction {
 
 
 	public static void specialSkill(int num1,int num2){
-		for(int i = 0;i < 310;i++){
-			int renum1 = num1 + i;
-			int renum2 = num2 + i;
+		for(int i = 0;i < 350;i++){
+		
 			if (num1 < 0 || num2 < 0) {
 				System.out.println("num1とnum2は0より大きい値にしてください。");
 				break;
@@ -22,41 +21,42 @@ public class GeekIntroduction {
 				System.out.println("num1はnum2より大きい値にしてください。");
 				break;
             	}
-            else if (300 < num1 || 300 < num2) {
+			else if (300 < num1 || 300 < num2) {
            		System.out.println("num1とnum2は300より小さい値にしてください。");
            		break;
             }
+			
             else{
-            	int mod3num1 = renum1 % 3;
-            	int mod3num2 = renum2 % 3;
-            	int mod5num1 = renum1 % 5;
-            	int mod5num2 = renum2 % 5;
+            	
+            	int mod3num1 = num1 % 3;
+            	int mod3num2 = num2 % 3;
+            	int mod5num1 = num1 % 5;
+            	int mod5num2 = num2 % 5;
+            	
             	if (mod3num1 == 0 && mod5num1 == 0) {
-            		System.out.println(renum1 + "は3の倍数かつ5の倍数です。");
-            		continue;
+            		System.out.println(num1 + "は3の倍数かつ5の倍数です。");
             	}
-            	else if (mod3num1 == 0 && mod5num1 != 0) {
-            		System.out.println(renum1 + "は3の倍数です。");
-            		continue;
+            		else if (mod3num1 == 0 && mod5num1 != 0) {
+            		System.out.println(num1 + "は3の倍数です。");
             		}
             		else if (mod3num1 != 0 && mod5num1 == 0) {
-            			System.out.println(renum1 + "は5の倍数です。");
-            			continue;
+            			System.out.println(num1 + "は5の倍数です。");
             		}
         
             	if (mod3num2 == 0 && mod5num2 == 0) {
-            		System.out.println(renum2 + "は3の倍数かつ5の倍数です。");
-            		continue;
-            	}
-            	else if (mod3num2 == 0 && mod5num2 != 0) {
-            		System.out.println(renum2 + "は3の倍数です。");
-            		continue;
-            	}
-            	else if (mod3num2 != 0 && mod5num2 == 0) {
-            		System.out.println(renum2 + "は5の倍数です。");
-            		continue;
+            		System.out.println(num2 + "は3の倍数かつ5の倍数です。");
+            		}
+            		else if (mod3num2 == 0 && mod5num2 != 0) {
+            		System.out.println(num2 + "は3の倍数です。");
+            		}
+            		else if (mod3num2 != 0 && mod5num2 == 0) {
+            		System.out.println(num2 + "は5の倍数です。");
                 }
             }
+		
+			num1++;
+			num2++;
+			
 		}
 	}
 }
